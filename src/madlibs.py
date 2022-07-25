bd45ce0efc72a286efc453beb00f21f6e38ce22c
nehaummareddy <nehaummareddy@gmail.com>
2022-07-25 23:00:51 +0530

    Create madlibs.py (#1)

---
+++ src/madlibs.py
@@ -0,0 +1,6 @@
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+madlib = f"This the most {adj} day! your would like to {verb1} \
+while you also like to {verb2}. You should live like you are {famous_person}"
